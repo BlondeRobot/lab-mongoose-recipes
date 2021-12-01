6be71dd4677f96eb02bef2087b70e5e6411699ba
Alicia Kmak <alicia.kmak@gmail.com>
2021-12-01 19:26:02 +0100

    add iterations 4,5,6. Lab completed, everything working

--- index.js
+++ index.js
@@ -19,21 +19,40 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany();
   })
-  // .then(() => {
-  //   Recipe.create({
-  //     title: "Spaghetti",
-  //     level: "Easy Peasy",
-  //     cuisine: "Italian",
-  //   }).then(() => {
-  //     console.log("done");
-  //   });
-  // })
-  // .then(() => Recipe.insertMany(data))
-  // .then((recipes) => {
-  //   recipes.forEach((recipe) => {
-  //     console.log(recipe.title)
-  //     })
-  //   })
+  .then(() => {
+    Recipe.create({
+      title: "Spaghetti",
+      level: "Easy Peasy",
+      cuisine: "Italian",
+    }).then((myRecipe) => {
+      console.log(myRecipe.title);
+    });
+  })
+  .then(() => Recipe.insertMany(data))
+  .then((recipes) => {
+    recipes.forEach((recipe) => {
+      console.log(recipe.title)
+      })
+    })
+  .then(() => {
+    Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese"},
+      { duration: 100},
+      { new: true}
+      );
+  })
+  .then((durationUpdated) => {
+    console.log("updated duration", durationUpdated);
+  })
+  .then(() => {
+    Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then((recipeDeleted) => {
+    console.log("Carrot Cake deleted", recipeDeleted)
+  })
+  .then(() => {
+    mongoose.connection.close();
+  })
   .catch((error) => {
     console.error("Error connecting to the database", error);
   });
